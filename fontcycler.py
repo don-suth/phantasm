95f20a57d013eab7fe806cfd81f726260076e9c4
Donald Sutherland <donald@sutherland.id.au>
2024-07-03 20:43:10 +0800

    Add a font tested

---
+++ fontcycler.py
@@ -0,0 +1,35 @@
+from baseeffect import BaseEffect
+from rgbmatrix import graphics
+import os
+
+SHOWCASE_TEXT = "Hello, Unigames!"
+
+
+class FontCycler(BaseEffect):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fonts = []
+		for filename in os.listdir("fonts"):
+			if filename.endswith(".bdf"):
+				self.fonts.append(filename)
+		self.current_font = graphics.Font()
+		self.x_offset = self.matrix.width
+		self.y_offset = self.matrix.height - 1
+		self.text_colour = graphics.Color(255, 255, 255)
+		self.next_font()
+		
+	def next_font(self):
+		if len(self.fonts) > 0:
+			self.current_font.LoadFont(f"fonts/{self.fonts.pop(0)}")
+			self.x_offset = self.matrix.width
+		else:
+			self.done = True
+			
+	def tick(self, canvas):
+		text_length = graphics.DrawText(
+			canvas, self.current_font, self.x_offset, self.y_offset,
+			self.text_colour, SHOWCASE_TEXT
+		)
+		self.x_offset -= 1
+		if self.x_offset + text_length < 0:
+			self.next_font()
