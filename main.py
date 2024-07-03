d82d84e8cf48d9220adae0d707b0d194f58c58cb
Donald Sutherland <donald@sutherland.id.au>
2024-07-03 20:45:13 +0800

    Add a font tester

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from controller import MatrixController
 from pingpongeffect import PingPongEffect
+from fontcycler import FontCycler
 
 
 if __name__ == "__main__":
 	controller = MatrixController()
-	controller.add_to_layers(PingPongEffect)
-	controller.add_to_layers(PingPongEffect)
-	controller.add_to_layers(PingPongEffect)
-	controller.add_to_layers(PingPongEffect)
-	controller.add_to_layers(PingPongEffect)
-	controller.add_to_layers(PingPongEffect)
-	controller.add_to_layers(PingPongEffect)
-	controller.add_to_layers(PingPongEffect)
+	controller.add_to_layers(FontCycler)
 	controller.run()
