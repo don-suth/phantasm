3476163b0a0bd66f526215c14469e1f90d1e4664
Donald Sutherland <donald@sutherland.id.au>
2024-07-09 16:51:46 +0800

    Alternative approach

--- async_main.py
+++ async_main.py
@@ -3,7 +3,6 @@ from layers.connection_status_layer import ConnectionStatusLayer
 from layers.clock_layer import ClockLayer
 import asyncio
 import websockets
-from websockets.client import connect as ws_connect
 
 
 async def main():
@@ -12,7 +11,7 @@ async def main():
 	clock_layer = await controller.add_to_layers(ClockLayer)
 	connection_status_layer = await controller.add_to_layers(ConnectionStatusLayer)
 	contoller_run_task = asyncio.create_task(controller.run())
-	async for websocket in ws_connect("ws://localhost:8765"):
+	async for websocket in websockets.connect("ws://localhost:8765"):
 		try:
 			# Connected
 			print("ws connected")
@@ -20,20 +19,12 @@ async def main():
 			async for message in websocket:
 				# Process message
 				print(message)
-		except websockets.exceptions.ConnectionClosed:
-			# If the connection was closed unexpectedly: retry in 2 seconds
-			print("ws reconnecting")
-			await connection_status_layer.set_reconnecting()
-			await asyncio.sleep(2)
-			continue
 		except websockets.exceptions.WebSocketException:
 			print("ws failed")
 			# All other connection problems: retry in 10 seconds
 			await connection_status_layer.set_failed()
 			await asyncio.sleep(10)
-			await connection_status_layer.set_reconnecting()
-			continue
-		except Exception:
-			raise
+		await connection_status_layer.set_reconnecting()
+		await asyncio.sleep(2)
 
 asyncio.run(main())
--- test_websocket_server.py
+++ test_websocket_server.py
@@ -1,12 +1,13 @@
-from websockets.server import serve
+from websockets import serve, WebSocketServerProtocol
 import asyncio
 
 
-async def echo(websocket):
+async def echo(websocket: WebSocketServerProtocol):
 	print("Someone connected")
 	# Close the connection in 10 seconds
 	async def auto_close():
 		await asyncio.sleep(10)
+		print("closing")
 		await websocket.close()
 	close_task = asyncio.create_task(auto_close())
 	async for message in websocket:
