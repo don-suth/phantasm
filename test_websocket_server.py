21ef466d856bd9501be57ee01f07c943c7a9b590
Donald Sutherland <donald@sutherland.id.au>
2024-07-09 16:14:36 +0800

    Test auto closing of connection

--- test_websocket_server.py
+++ test_websocket_server.py
@@ -4,6 +4,11 @@ import asyncio
 
 async def echo(websocket):
 	print("Someone connected")
+	# Close the connection in 10 seconds
+	async def auto_close():
+		await asyncio.sleep(10)
+		await websocket.close()
+	close_task = asyncio.create_task(auto_close())
 	async for message in websocket:
 		print(message)
 		await websocket.send(message)
