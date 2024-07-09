294290abe8e8acd15203e1a1ef1e997f6686a22a
Donald Sutherland <donald@sutherland.id.au>
2024-07-09 23:35:35 +0800

    Add date to the clock

--- layers/clock_layer.py
+++ layers/clock_layer.py
@@ -13,6 +13,8 @@ class ClockLayer(BaseLayer):
 		super().__init__(*args, **kwargs)
 		self.font = graphics.Font()
 		self.font.LoadFont("my_fonts/spleen-12x24.bdf")
+		self.small_font = graphics.Font()
+		self.small_font.LoadFont("fonts/5x7.bdf")
 		self.text_colour = graphics.Color(255, 255, 0)  # Yellow
 
 	def tick(self, canvas):
@@ -46,6 +48,10 @@ class ClockLayer(BaseLayer):
 		graphics.DrawText(
 			canvas, self.font, 34, 23, self.text_colour,
 			text=f"{current_min:0>2}"
+		)
+		graphics.DrawText(
+			canvas, self.small_font, 1, 1, self.text_colour,
+			text=f"{current_time:%a %d %B}"
 		)
 		
 		
