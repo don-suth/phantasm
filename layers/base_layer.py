ea266bdb056cccb776976bab00a088b08b75d9b4
donsuth <donald@sutherland.id.au>
2025-03-27 12:33:20 +0800

    Update some stuff for the base layer

--- layers/base_layer.py
+++ layers/base_layer.py
@@ -6,7 +6,7 @@ class BaseLayer:
 		self.matrix = matrix
 		self.done = False
 	
-	def tick(self, canvas: FrameCanvas):
+	def tick(self, canvas: FrameCanvas, x_offset: int = 0, y_offset: int = 0):
 		"""
 		The main logic of the effect should be applied here.
 		"""
