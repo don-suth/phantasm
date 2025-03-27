d8c5f1aa1119a465555c40b9c771083db4c59b3b
donsuth <donald@sutherland.id.au>
2025-03-27 15:51:27 +0800

    Add a Base Transition

---
+++ layers/transition_base.py
@@ -0,0 +1,15 @@
+from rgbmatrix import RGBMatrix, FrameCanvas
+
+
+class BaseTransition:
+	def __init__(self, matrix: RGBMatrix, from_layer, to_layer, *args, **kwargs):
+		self.matrix = matrix
+		self.from_layer = from_layer
+		self.to_layer = to_layer
+		self.done = False
+
+	def tick(self, canvas: FrameCanvas, x_offset: int = 0, y_offset: int = 0):
+		"""
+		The main logic of the transition should be applied here.
+		"""
+		raise NotImplemented("Implement your own logic here.")
