f50c042437fb9c1abc9791a467da13272b3d20f7
donsuth <donald@sutherland.id.au>
2025-04-09 12:54:22 +0800

    Few adjustments to the alert layer

--- layers/alert_layer.py
+++ layers/alert_layer.py
@@ -23,8 +23,10 @@ class AlertLayer(BaseLayer):
 		self.smaller_font = graphics.Font()
 		self.smaller_font.LoadFont("fonts/6x9.bdf")
 
+		self.message_width = 0
 		self.message_x_offset = 0
 		self.message_scrolling = False
+		self.message_wrap = False
 
 		self.location_x_offset = 0
 		self.text_change_task = asyncio.create_task(self.text_changer())
@@ -38,13 +40,15 @@ class AlertLayer(BaseLayer):
 		while True:
 			for text in text_strings:
 				self.message = text
-				if len(text) * 6 > 64:
+				self.message_width = len(text) * 6
+				if self.message_width > 64:
 					self.message_x_offset = 63
 					self.message_scrolling = True
+					self.message_wrap = True
 				else:
 					self.message_x_offset = (64 - (len(text) * 6)) // 2
 					self.message_scrolling = False
-				await asyncio.sleep(5)
+				await asyncio.sleep(30)
 
 	async def location_changer(self):
 		locations = [
@@ -57,18 +61,40 @@ class AlertLayer(BaseLayer):
 				await asyncio.sleep(5)
 
 	def tick(self, canvas: FrameCanvas, frame_x_offset: int = 0, frame_y_offset: int = 0):
+		# Draw the message
 		graphics.DrawText(
 			canvas, self.small_font, self.message_x_offset + frame_x_offset, frame_y_offset + 10, graphics.Color(255, 255, 255),
 			text=self.message,
 		)
+
+		# If the message is wrapped, draw it again behind it
+		if self.message_wrap:
+			graphics.DrawText(
+				canvas, self.small_font, self.message_x_offset + frame_x_offset + self.message_width + 20, frame_y_offset + 10,
+				graphics.Color(255, 255, 255),
+				text=self.message,
+			)
+
+		# Draw the location
 		graphics.DrawText(
 			canvas, self.font, self.location_x_offset + frame_x_offset, frame_y_offset + 25, graphics.Color(255, 255, 255),
 			text=self.location
 		)
+
+		# Draw the word "ENTRANCE"
 		graphics.DrawText(
 			canvas, self.smaller_font, 8 + frame_x_offset, frame_y_offset + 32, graphics.Color(255, 255, 0),
 			text="ENTRANCE"
 		)
+
+		# Scroll the text to the left
 		if self.message_scrolling:
 			self.message_x_offset -= 1
+
+			# If the text is wrapped, stop it scrolling once the second instance
+			# reaches the left edge
+			if self.message_wrap:
+				if self.message_x_offset == -self.message_width - 20:
+					self.message_scrolling = False
+
 		return self
