095a29144ecd2695a0891c67355e4965578eac20
donsuth <donald@sutherland.id.au>
2025-04-09 12:18:38 +0800

    Test rotating through different texts

--- layers/smash_cut_to_text_transition.py
+++ layers/smash_cut_to_text_transition.py
@@ -1,6 +1,14 @@
 import asyncio
+import random
 from layers.transition_base import BaseTransition
 from rgbmatrix import FrameCanvas, RGBMatrix, graphics
+
+TEXT_NOTIFICATIONS = (
+	("Ring", "Ring!"),
+	("Ding", "Dong!"),
+	("Hello", "there"),
+	("Look", "at me"),
+)
 
 
 class SmashCutTextTransition(BaseTransition):
@@ -10,18 +18,25 @@ class SmashCutTextTransition(BaseTransition):
 		self.font.LoadFont("fonts/9x15B.bdf")
 		self.countdown_task = asyncio.create_task(self.delay_transition())
 		self.transition_state = 0
+		self.display_texts = TEXT_NOTIFICATIONS[0]
 
 	async def delay_transition(self):
-		# Initially show nothing
-		await asyncio.sleep(0.01)
+		while True:
+			# Initially show nothing
+			await asyncio.sleep(0.01)
 
-		# Show first line of text
-		self.transition_state = 1
-		await asyncio.sleep(0.3)
+			# Show first line of text
+			self.transition_state = 1
+			await asyncio.sleep(0.3)
 
-		# Show second line of text
-		self.transition_state = 2
-		await asyncio.sleep(2)
+			# Show second line of text
+			self.transition_state = 2
+			await asyncio.sleep(2)
+
+			# Reset for testing purposes
+			self.display_texts = random.choice(TEXT_NOTIFICATIONS)
+			self.transition_state = 0
+			await asyncio.sleep(1)
 
 		# Finish the transition
 		self.transition_state = -1
@@ -32,12 +47,12 @@ class SmashCutTextTransition(BaseTransition):
 			if self.transition_state >= 1:
 				graphics.DrawText(
 					canvas, self.font, frame_x_offset + 7, frame_y_offset + 13,
-					graphics.Color(255, 255, 255), text="Ring"
+					graphics.Color(255, 255, 255), text=self.display_texts[0],
 				)
 			if self.transition_state >= 2:
 				graphics.DrawText(
 					canvas, self.font, frame_x_offset + 18, frame_y_offset + 27,
-					graphics.Color(255, 255, 255), text="Ring!"
+					graphics.Color(255, 255, 255), text=self.display_texts[1],
 				)
 			return self
 		else:
