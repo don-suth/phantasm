a76240de8f726d4485b8271c9e994344343a79ef
donsuth <donald@sutherland.id.au>
2025-04-09 12:10:48 +0800

    Certain parts of the text appear at different times

--- layers/smash_cut_to_text_transition.py
+++ layers/smash_cut_to_text_transition.py
@@ -9,23 +9,36 @@ class SmashCutTextTransition(BaseTransition):
 		self.font = graphics.Font()
 		self.font.LoadFont("fonts/9x15B.bdf")
 		self.countdown_task = asyncio.create_task(self.delay_transition())
-		self.transition_complete = False
+		self.transition_state = 0
 
 	async def delay_transition(self):
+		# Initially show nothing
+		await asyncio.sleep(0.01)
+
+		# Show first line of text
+		self.transition_state = 1
+		await asyncio.sleep(0.3)
+
+		# Show second line of text
+		self.transition_state = 2
 		await asyncio.sleep(2)
-		self.transition_complete = True
+
+		# Finish the transition
+		self.transition_state = -1
 
 	def tick(self, canvas: FrameCanvas, frame_x_offset: int = 0, frame_y_offset: int = 0):
-		if self.transition_complete is False:
+		if self.transition_state >= 0:
 			canvas.Clear()
-			graphics.DrawText(
-				canvas, self.font, frame_x_offset + 7, frame_y_offset + 13,
-				graphics.Color(255, 255, 255), text="Ring"
-			)
-			graphics.DrawText(
-				canvas, self.font, frame_x_offset + 18, frame_y_offset + 27,
-				graphics.Color(255, 255, 255), text="Ring!"
-			)
+			if self.transition_state >= 1:
+				graphics.DrawText(
+					canvas, self.font, frame_x_offset + 7, frame_y_offset + 13,
+					graphics.Color(255, 255, 255), text="Ring"
+				)
+			if self.transition_state >= 2:
+				graphics.DrawText(
+					canvas, self.font, frame_x_offset + 18, frame_y_offset + 27,
+					graphics.Color(255, 255, 255), text="Ring!"
+				)
 			return self
 		else:
 			return self.to_layer.tick(canvas, frame_x_offset, frame_y_offset)
